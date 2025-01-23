74967bba7e3d0f925272ecba941ed2e4e240a2b1
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-01-23 18:04:37 +0900

    db변경-postgresql

--- drf_homework3/drf_homework3/settings.py
+++ drf_homework3/drf_homework3/settings.py
@@ -86,12 +86,24 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#   }
+#}
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homework_db", #db이름
+        "USER": "postgres", 
+        "PASSWORD": "7517",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 AUTH_USER_MODEL = "user.CustomUser"
 
